01b085efa83b47162ff91c093c0923dbc3321c8c
Wilson Souto <wilsonsouto@outlook.com.br>
2025-02-16 08:56:49 -0300

    Upgrade Angular dependencies to version 19.1.x and update app.component.ts formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
   public title: string = 'Angular Starter Kit';
